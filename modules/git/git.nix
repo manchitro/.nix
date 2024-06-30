a8f5bfc66e3c0a58003007b79e1b01c7cacef46e
Sazid <initialsaremsu@gmail.com>
2024-06-30 17:35:42 +0600

    kitty

--- modules/git/git.nix
+++ modules/git/git.nix
@@ -3,7 +3,7 @@
 {
   programs.git = {
     enable = true;
-    userName = "John Doe";
-    userEmail = "johndoe@example.com";
+    userName = "Sazid";
+    userEmail = "initialsaremsu@gmail.com";
   };
 }
