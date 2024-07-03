610bd670fae09e0a92861dccce17c3a64f42f951
Sazid <initialsaremsu@gmail.com>
2024-07-03 15:57:31 +0600

    added .ideavimrc

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -55,6 +55,7 @@
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
+    ".ideavimrc".source = ../../modules/.ideavimrc;
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
